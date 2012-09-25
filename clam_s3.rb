0760e68732471e37f24684f71ce54cf9bbb6071d
Kevin Mullin <kevin@kpmullin.com>
2012-09-24 21:37:57 -0700

    skel

--- clam_s3.rb
+++ clam_s3.rb
@@ -1,3 +1,41 @@
 #!/usr/bin/env ruby
 
+require 'optparse'
+require 'yaml'
 require 'aws/s3'
+
+class ClamS3
+
+  def initialize(options={})
+    options.merge!(YAML.load_file('config/amazon.yml'))
+    options.keys.each do |key|
+      options[(key.to_sym rescue key) || key] = options.delete(key)
+    end
+    @dry_run = options[:dry_run] || false
+    unless @dry_run
+      AWS::S3::Base.establish_connection!(
+        :access_key_id     => options[:access_key_id],
+        :secret_access_key => options[:secret_access_key],
+        :use_ssl => true
+      )
+      @bucket = AWS::S3::Bucket.find(options[:bucket])
+    end
+  end
+
+end
+
+options = {}
+OptionParser.new do |opt|
+
+  opt.banner = "Usage: #{File.basename($0)} [options]"
+  opt.separator ""
+  opt.separator "Options:"
+
+  opt.on('-v', '--verbose', 'Verbose') { options[:verbose] = true }
+  opt.on('-d', '--debug', 'Debug') { options[:debug] = true }
+  opt.on('-n', '--dry-run', 'Dry run') { options[:dry_run] = true }
+  opt.on('-b', '--bucket name', 'bucket name') { |name| options[:bucket] = name }
+
+end.parse!
+
+c = ClamS3.new(options)
